974c688c8dc386f0dd7963553e13b05c84655d54
Syeda_urooj <syedaaaurooj021@gmail.com>
2025-06-11 23:18:14 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,76 @@
+// CHAPTER: 06-09;
+// QUESTION 1:Arithematic operation;
+// let num = 10;
+// document.write("Result: " + "<br>");
+// document.write("The value of num is: " + num + "<br><br>");
+
+// document.write("The value of ++num is: " + (++num) + "<br>");
+// document.write("Now The value of num is:" + num + "<br><br>");
+
+// document.write("The value of num++ is: " + (num++) + "<br>");
+// document.write("Now The value of num is:" + num + "<br><br>");
+
+
+// document.write("The value of --num is: " + (--num) + "<br>");
+// document.write("Now The value of num is:" + num + "<br><br>");
+
+
+// document.write("The value of num-- is: " + (num--) + "<br>");
+// document.write("Now The value of num is:" + num + "<br><br>");
+// QUESTION 2: PRE POST INCREMENT DECREMENT;
+// var a = 2, b = 1;
+
+// var result = --a - --b + ++b + b--;
+// document.write("a is " + a + "<br>");
+// document.write("b is " + b + "<br>");
+// document.write("Result is " + result);
+// // QUESTION 3: GREETING;
+// let userName = prompt("What's your name?");
+// document.write("<h2>Welcome " + userName + "!</h2>");
+
+// QUESTION 4: MULTIPLICATION TABLE;
+// var tableNum = +prompt("Enter a number for multiplication table:", 8);
+// if (tableNum === 0) {
+//     tableNum = 8;
+// }
+// document.write("<h2>Multiplication Table of " + tableNum + "</h2>");
+// document.write(tableNum + " x 1 = " + (tableNum * 1) + "<br>");
+// document.write(tableNum + " x 2 = " + (tableNum * 2) + "<br>");
+// document.write(tableNum + " x 3 = " + (tableNum * 3) + "<br>");
+// document.write(tableNum + " x 4 = " + (tableNum * 4) + "<br>");
+// document.write(tableNum + " x 5 = " + (tableNum * 5) + "<br>");
+// document.write(tableNum + " x 6 = " + (tableNum * 6) + "<br>");
+// document.write(tableNum + " x 7 = " + (tableNum * 7) + "<br>");
+// document.write(tableNum + " x 8 = " + (tableNum * 8) + "<br>");
+// document.write(tableNum + " x 9 = " + (tableNum * 9) + "<br>");
+// document.write(tableNum + " x 10 = " + (tableNum * 10) + "<br>");
+// QUESTION 5: MARK SHEET FOR THREE SUBJECTS;
+// part a;
+// var subject1 = prompt("Enter name of subject 1:");
+// var subject2 = prompt("Enter name of subject 2:");
+// var subject3 = prompt("Enter name of subject 3:");
+// part b;
+// let totalMarksPersub = 100;
+// part c;
+// let marks1 = +prompt("Enter obtained marks for " + subject1 + ":");
+// part d;
+// let marks2 = +prompt("Enter obtained marks for " + subject2 + ":");
+// let marks3 = +prompt("Enter obtained marks for " + subject3 + ":");
+// part e:calculate total marks percentage and display in browser;
+// let totalObtained = marks1 + marks2 + marks3;
+// let totalMarks = 3 * totalMarksPersub;
+// let percentage1 = (marks1 / totalMarksPersub) * 100;
+// let percentage2 = (marks1 / totalMarksPersub) * 100;
+// let percentage3 = (marks1 / totalMarksPersub) * 100;
+// let totalPercentage = (totalMarks / totalObtained) * 100;
+// document.write("<h2>MARKSHEET<h2>");
+// document.write(subject1 + ":" + marks1 + "/100 (" + percentage1 + "/100" + "%)<br>");
+// document.write(subject2 + ":" + marks2 + "/100 (" + percentage2 + "/100" + "%)<br>");
+// document.write(subject3 + ":" + marks3 + "/100 (" + percentage3 + "/100" + "%)<br><br>");
+// document.write("Total Marks: " + totalMarks + "<br>");
+// document.write("Total Obtained: " + totalObtained + "<br>");
+// document.write("Overall Percentage: " + totalPercentage + "%");
+
+
+
+
